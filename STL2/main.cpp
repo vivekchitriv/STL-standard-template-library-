2f12781d054ff45fe03ad9a36fc671d7ed850c94
Vcc@916776 <vivekchitriv786@gmail.com>
2020-04-09 19:40:08 +0530

    hay gues this is second project using iterator

---
+++ STL2/main.cpp
@@ -0,0 +1,27 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 09-Apr-2020
+ *      Author: sunbam
+ */
+
+//c++ code to demonstrate the the working  of iterator begin() and end();
+
+#include<iostream>
+#include<vector>
+#include<iterator>
+using namespace std;
+
+int main()
+{
+	vector<int> ar={1,2,3,4,5,6};
+	//declaring iterator to a vector
+	vector<int> :: iterator ptr;
+	cout<<"Vector elements are::"<<endl;
+
+	for(ptr=ar.begin();ptr<ar.end();ptr++)
+		cout<<*ptr<<" ";
+	return 0;
+}
+
+
