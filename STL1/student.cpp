b707c2a81ffcd0ad329b19c0abd932fd1c51f86d
Vcc@916776 <vivekchitriv786@gmail.com>
2020-04-10 09:50:10 +0530

    addedd two files from dev1

---
+++ STL1/student.cpp
@@ -0,0 +1,18 @@
+/*
+ * student.cpp
+ *
+ *  Created on: 10-Apr-2020
+ *      Author: sunbam
+ */
+#include<iostream>
+#include "student.h"
+using namespace std;
+student::student() {
+	// TODO Auto-generated constructor stub
+cout<<"hello sir";
+}
+
+student::~student() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ STL1/student.h
@@ -0,0 +1,17 @@
+/*
+ * student.h
+ *
+ *  Created on: 10-Apr-2020
+ *      Author: sunbam
+ */
+
+#ifndef STUDENT_H_
+#define STUDENT_H_
+
+class student {
+public:
+	student();
+	virtual ~student();
+};
+
+#endif /* STUDENT_H_ */
